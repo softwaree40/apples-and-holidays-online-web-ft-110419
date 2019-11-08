070134be6b6dbf49f3268a51d27d1c93df9093b8
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-08 18:43:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   #  binding.pry
   holiday_hash.each do |seasons ,values|
      if seasons == :fall
-         values[:thanksgiving] << holiday_name
+         values[:thanksgiving] = holiday_name
         #binding.pry
       end
     end
