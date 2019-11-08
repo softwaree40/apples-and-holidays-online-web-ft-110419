fe5518ddf9a69bebb75f0f31895938b8e9e1a649
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-08 19:06:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,9 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-   
+   holiday_hash.each do |season,supply|
+     
+   end
 end
 
 def all_supplies_in_holidays(holiday_hash)
